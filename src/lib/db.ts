375e3a9e4712fb5b67cfe3133caa87293197dff4
YchXD <ych08.cpm@gmail.com>
2025-10-09 23:34:33 +0700

    add port to pool conn

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST!,
+  user: process.env.DB_USER!,
+  port: parseInt(process.env.DB_PORT!, 10),
+  password: process.env.DB_PASS!,
+  database: process.env.DB_NAME!,
   waitForConnections: true,
   connectionLimit: 1000000000, 
   queueLimit: 0,
